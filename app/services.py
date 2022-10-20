fe80b96b8c45a41ac2c8559a4b912a92cef05c20
SeergeyL <pstydy@inbox.ru>
2022-10-21 00:21:16 +0300

    add language detection

--- app/services.py
+++ app/services.py
@@ -1,5 +1,6 @@
 from googletrans import Translator
-from config import TranslationMode, DESTINATION_LANGUAGE, MESSAGE_MARKER
+
+import config
 from labels import RESPONSE_TEXT
 
 
@@ -7,23 +8,34 @@ class TranslationService:
     def __init__(self, service_urls, **kwargs):
         self.translator = Translator(service_urls=service_urls, **kwargs)
 
+    def check_language(self, message: str) -> bool:
+        lang = self.translator.detect(message)
+        is_destination_lang = lang.lang == config.DESTINATION_LANGUAGE
+        lang_confidence = lang.confidence > config.CONFIDENCE_THRESHOLD
+        if  is_destination_lang and lang_confidence:
+            return True
+        return False
+
     def translate(
         self,
         text: str,
-        mode: TranslationMode,
+        mode: config.TranslationMode,
     ) -> None | str:
-        if mode == TranslationMode.SYNC:
+        if self.check_language(text):
+            return None
+
+        if mode == config.TranslationMode.SYNC:
             translated = self.translator.translate(
                 text,
-                dest=DESTINATION_LANGUAGE
+                dest=config.DESTINATION_LANGUAGE
             )
             return RESPONSE_TEXT['translated'].format(text=translated.text)
 
-        if MESSAGE_MARKER not in text:
+        if config.MESSAGE_MARKER not in text:
             return None
 
         translated = self.translator.translate(
             text,
-            dest=DESTINATION_LANGUAGE
+            dest=config.DESTINATION_LANGUAGE
         )
         return RESPONSE_TEXT['translated'].format(text=translated.text)
